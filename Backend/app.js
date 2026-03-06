0df7201b0b4da31cff5bc74ce901a06d819bebdb
KavindiSirimanna <sirimannakavindi@gmail.com>
2026-03-06 17:47:43 +0530

    n

--- Backend/app.js
+++ Backend/app.js
@@ -8,5 +8,5 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("backen is working"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on " + PORT));
