bea72e475e6e795260a7d7e5313601726467a343
KavindiSirimanna <sirimannakavindi@gmail.com>
2026-03-06 17:41:32 +0530

    backend changes

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Backend is running!"));
+app.get("/", (req, res) => res.send("backen is working"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log("Server running on " + PORT));
