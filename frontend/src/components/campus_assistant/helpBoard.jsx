41dddc7dcb5d921d147a8ca8703a4942249378d3
Oshadi <oshadisubhasinghe15@gmail.com>
2026-03-20 12:33:45 +0530

    Student

--- frontend/src/components/campus_assistant/helpBoard.jsx
+++ frontend/src/components/campus_assistant/helpBoard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from "react";
-import React, { useEffect, useState } from "react";
 import { getHelpRequests, deleteHelpRequest } from "../../services/helpApi";
 
 export default function HelpBoard() {
